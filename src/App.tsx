d68bec6328694c15f4dfe3d0685ad32eefd89d62
lubov-nefed <love.nefed@gmail.com>
2024-06-22 14:39:35 +0300

    feat: add icons to primary btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,120 @@
 import { useState } from "react";
 import "./App.css";
 import { Button } from "./components/Button/Button";
+import iconLeftAdd from "../src/assets/icons/icon-left-add.svg";
+import iconRightArrow from "../src/assets/icons/icon-right-arrow.svg";
+import iconSettings from "../src/assets/icons/icon-settings.svg";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   return (
     <>
       App
+      <br />
+      {/* primary medium*/}
       <Button
-        text={"M Default"}
-        size="medium"
-        style="primary"
+        size={"medium"}
+        style={"primary"}
         loading={false}
         disabled={false}
+        type={{ noIcon: { text: "M Default" } }}
+        onClick={() => {}}
         /* onClick={() => setIsLoading(!isLoading)} */
       />
       <Button
-        text={"M Default"}
-        size="medium"
-        style="primary"
-        loading={true}
-        disabled={false}
-      />
-      <Button
-        text={"M Default"}
         size="medium"
         style="primary"
         loading={false}
-        disabled={true}
+        disabled={false}
+        type={{
+          withIcon: {
+            iconPosition: "left",
+            iconSrc: iconLeftAdd,
+            text: "M Default",
+          },
+        }}
+        onClick={() => {}}
+      />
+      <Button
+        size="medium"
+        style="primary"
+        loading={false}
+        disabled={false}
+        type={{
+          withIcon: {
+            iconPosition: "right",
+            iconSrc: iconRightArrow,
+            text: "M Default",
+          },
+        }}
+        onClick={() => {}}
+      />
+      <Button
+        size="medium"
+        style="primary"
+        loading={false}
+        disabled={false}
+        type={{ iconOnly: { iconSrc: iconSettings } }}
+        onClick={() => {}}
       />
       <br />
+      {/* primary Big*/}
+      {/* <Button
+        text={"Big Default"}
+        size="big"
+        style="primary"
+        loading={false}
+        disabled={false}
+        type={"noIcon"}
+        onClick={() => {}}
+      />
       <Button
         text={"Big Default"}
         size="big"
-        style="secondary"
-        loading={isLoading}
-        disabled={true}
+        style="primary"
+        loading={true}
+        disabled={false}
+        onClick={() => {}}
+        type={"noIcon"}
       />
       <Button
-        text={"Add to Cart"}
-        size="small"
-        style="tertiary"
-        loading={isLoading}
+        text={"Big Default"}
+        size="big"
+        style="primary"
+        loading={false}
+        disabled={true}
+        type={"noIcon"}
+        onClick={() => {}}
       />
+      <br /> */}
+      {/* primary Small*/}
+      {/* <Button
+        text={"S Default"}
+        size="small"
+        style="primary"
+        loading={false}
+        disabled={false}
+        type={"noIcon"}
+        onClick={() => {}}
+      />
+      <Button
+        text={"S Default"}
+        size="small"
+        style="primary"
+        loading={true}
+        disabled={false}
+        type={"noIcon"}
+        onClick={() => {}}
+      />
+      <Button
+        text={"S Default"}
+        size="small"
+        style="primary"
+        loading={false}
+        disabled={true}
+        type={"noIcon"}
+        onClick={() => {}}
+      /> */}
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,66 @@
 import "./Button.css";
 
-function Button(props: {
-  text: string;
+interface IButtonIconProps {
+  position: "left" | "right";
+  src: string;
+}
+
+interface IButtonProps {
   size: "medium" | "big" | "small";
   style: "primary" | "secondary" | "tertiary";
+  type:
+    | { noIcon: { text: string } }
+    | {
+        withIcon: {
+          iconPosition: "left" | "right";
+          iconSrc: string;
+          text: string;
+        };
+      }
+    | { iconOnly: { iconSrc: string } };
   loading: boolean;
   disabled: boolean;
   onClick: () => void;
-  leftIcon;
-  rightIcon;
-}) {
+}
+
+const ButtonIcon: React.FC<IButtonIconProps> = (props) => {
+  return (
+    <img
+      className={`button-icon button-icon--${props.position}`}
+      src={props.src}
+    />
+  );
+};
+
+const Button: React.FC<IButtonProps> = (props) => {
   const className =
     `button button-${props.size} ` +
     (props.loading
       ? `button-${props.style}--loading`
       : `button-${props.style}`);
+  const buttonText =
+    ("noIcon" in props.type && props.type.noIcon.text) ||
+    ("withIcon" in props.type && props.type.withIcon.text);
   return (
     <button
       className={className}
       onClick={props.onClick}
       disabled={props.disabled}
     >
-      {props.text}
+      {"withIcon" in props.type &&
+        props.type.withIcon.iconPosition === "left" && (
+          <ButtonIcon position={"left"} src={props.type.withIcon.iconSrc} />
+        )}
+      <span className="button-text">{buttonText}</span>
+      {"withIcon" in props.type &&
+        props.type.withIcon.iconPosition === "right" && (
+          <ButtonIcon position={"right"} src={props.type.withIcon.iconSrc} />
+        )}
+      {"iconOnly" in props.type && (
+        <img className="button-icon" src={props.type.iconOnly.iconSrc} />
+      )}
     </button>
   );
-}
+};
 
 export { Button };
